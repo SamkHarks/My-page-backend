567a2774ed99c03b7d46395a71228800fc628e14
SamkHarks <samikh90@gmail.com>
2025-04-07 09:25:04 +0300

    add cors & middleware for unknownEndPoint

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import skillsRouter from './routes/skills.js';
+import cors from 'cors';
+import { unknownEndpoint } from './middlewares/unknownEndPoint/unknownEndPoint.js';
+
+
 const app = express();
 
-// Middleware
+const corsOptions = {
+  methods: ['GET'], // Currently no other methods are supported
+  origin: ['http://localhost:3000', 'https://samis-portfolio.onrender.com']
+}
+
+// Middlewares
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
 app.use('/api/skills', skillsRouter);
+
+// After all routes middleware
+app.use(unknownEndpoint);
 
 
 const PORT =  process.env['PORT'] || 3001;
