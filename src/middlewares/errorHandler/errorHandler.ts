9f806619eeae5f3e1351b05fb9c10505233becdb
SamkHarks <samikh90@gmail.com>
2025-04-08 21:31:19 +0300

    handle zod validation errors

--- src/middlewares/errorHandler/errorHandler.ts
+++ src/middlewares/errorHandler/errorHandler.ts
@@ -1,6 +1,17 @@
 import { Request, Response, NextFunction } from 'express';
+import { z } from 'zod';
 
 
 export const errorHandler = (error: Error, _request: Request, response: Response, _next: NextFunction) => {
+
+  if (error instanceof z.ZodError) {
+    // Handle Zod validation errors
+    response.status(400).json({
+      error: 'Validation error',
+      issues: error.issues,
+    });
+    return;
+  }
+
   response.status(500).json({ error: error.message || 'Internal server error' });
 };
