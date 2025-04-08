d16b5ca8b7695d16aaaa69ce7c59167c98a5c96a
SamkHarks <samikh90@gmail.com>
2025-04-08 21:25:47 +0300

    add simple request logger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import cors from 'cors';
 import { unknownEndpoint } from '@/middlewares/unknownEndPoint/unknownEndPoint.js';
 import { logErrors } from '@/middlewares/logErrors/logErrors.js';
 import { errorHandler } from '@/middlewares/errorHandler/errorHandler.js';
+import { requestLogger } from '@/middlewares/requestLogger/requestLogger.js';
 
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 // Middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(requestLogger);
 
 // Routes
 app.use(router);
