7a643706364cc1d062740e3272935bc10498617a
SamkHarks <samikh90@gmail.com>
2025-04-08 21:30:52 +0300

    simplify contact route by using SafeRouter

--- src/routes/controllers/contact/contact.ts
+++ src/routes/controllers/contact/contact.ts
@@ -1,34 +1,23 @@
-import express, { Request, Response } from 'express';
+import { Request, Response } from 'express';
 import { contactSchema } from '@/routes/controllers/contact/types.js';
-import { z } from 'zod';
 import { EmailOptions, sendEmail } from '@/services/emailService.js';
+import { SafeRouter } from '@/utils/SafeRouter.js';
 
 
-const router = express.Router();
+const safeRouter = new SafeRouter();
 
-router.post('/', async (req: Request, res: Response) => {
-  try {
-    const validatedData = contactSchema.parse(req.body);
-    const { name, email, message } = validatedData;
 
-    const emailOptions: EmailOptions = {
-      to: email,
-      subject: `New message from ${name}`,
-      text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    };
-    await sendEmail(emailOptions);
-    res.status(200).json({ message: 'Email sent successfully!' });
-  } catch (error: unknown) {
-    if (error instanceof z.ZodError) {
-      res.status(400).json({ error: error.errors });
-    }
-    else if (error instanceof Error) {
-      res.status(500).json({ error: error.message });
-    } else {
-      res.status(500).json({ error: 'An unknown error occurred' });
-    }
-  }
+safeRouter.post('/', async (req: Request, res: Response) => {
+  const validatedData = contactSchema.parse(req.body);
+  const { name, email, message } = validatedData;
+  const emailOptions: EmailOptions = {
+    to: email,
+    subject: `New message from ${name}`,
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+  };
+  await sendEmail(emailOptions);
+  res.status(200).json({ message: 'Email sent successfully!' });
 });
 
 
-export default router;
+export default safeRouter.getRouter();
