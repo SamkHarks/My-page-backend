2ac604706b8e41d8c938c456fef0baf4162acde6
SamkHarks <samikh90@gmail.com>
2025-04-22 17:07:22 +0300

    log complete error in dev mode

--- src/middlewares/logErrors/logErrors.ts
+++ src/middlewares/logErrors/logErrors.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 export const logErrors = (error: Error, _request: Request, _response: Response, next: NextFunction) => {
   if (process.env['NODE_ENV'] === 'development') {
     // Log full stack trace in development
-    console.error(error.stack);
+    console.error(error);
   } else {
     // TODO: Log error to external service in production
   }
