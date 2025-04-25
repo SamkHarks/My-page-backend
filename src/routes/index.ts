407b17235821bfd206678913d7e24e16b300c892
SamkHarks <samikh90@gmail.com>
2025-04-25 15:29:07 +0300

    add health check controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import skillsRouter from '@/routes/controllers/skills/skills.js';
 import contactRouter from '@/routes/controllers/contact/contact.js';
+import healthRouter from '@/routes/controllers/health/health.js';
 
 const router = Router();
 
 router.use('/api/skills', skillsRouter);
 router.use('/api/contact', contactRouter);
+router.use('/api/health', healthRouter);
 
 export default router;
